ed9e1424da9b56a72b71de2dfc6f1845e1a6c99a
Amit Das <85489180+amitdas707@users.noreply.github.com>
2024-09-26 16:18:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const passwordBox = document.getElementById("Password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const number = "1234567890";
+const symbol = "@#$%^&*()_+~|{}[]/-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard!");
+}
+
+
+
+
